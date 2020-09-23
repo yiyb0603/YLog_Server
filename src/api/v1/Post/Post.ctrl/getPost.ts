fa3859a334f7c33081df93684a0101ea7cf78633
yiyb0603 <yiyb0603@gmail.com>
2020-09-23 14:03:32 +0900

    [ADD] getPost comment length

--- src/api/v1/Post/Post.ctrl/getPost.ts
+++ src/api/v1/Post/Post.ctrl/getPost.ts
@@ -3,11 +3,15 @@ import { Request, Response } from 'express';
 import { getRepository, Repository } from 'typeorm';
 import ColorConsole from '../../../../lib/ColorConsole';
 import { handleFailed, handleSuccess } from '../../../../lib/Response';
+import { Comment } from '../../../../entity/Comment';
 
 export default async (request: Request, response: Response) => {
 	try {
 		const idx = Number(request.params.idx);
 		const postRepository: Repository<Post> = getRepository(Post);
+		const commentRepository: Repository<Comment> = getRepository(Comment);
+
+		let commentLength: number = 0;
 
 		if (!Number.isInteger(idx)) {
 			ColorConsole.red(`[ERROR 400] 검증 오류입니다.`);
@@ -19,6 +23,16 @@ export default async (request: Request, response: Response) => {
 				idx,
 			},
 		});
+
+		const count: number = await commentRepository.count({
+			where: {
+				post_idx: idx,
+			},
+		});
+
+		commentLength += count;
+		post.comment_length = commentLength;
+		commentLength = 0;
 
 		if (!post) {
 			ColorConsole.red(`[ERROR 404] 존재하지 않는 글입니다.`);
