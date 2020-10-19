00c926a029b76699797af0ddb81adaf132bf23fb
yiyb0603 <yiyb0603@gmail.com>
2020-10-20 08:17:02 +0900

    [FIX] request error

--- src/lib/validation/Member/allowMember.ts
+++ src/lib/validation/Member/allowMember.ts
@@ -4,7 +4,7 @@ import validate from '../validate';
 
 export const validateAllowMember = (request: Request, response: Response) => {
 	const scheme: Joi.ObjectSchema = Joi.object().keys({
-		memberId: Joi.string().required(),
+		memberIdx: Joi.number().integer().required(),
 	});
 
 	return validate(request, response, scheme);
